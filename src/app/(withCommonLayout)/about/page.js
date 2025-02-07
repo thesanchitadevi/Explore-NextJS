1f8881eae0ea31539e94fb3420a8b7b20bc7ccfd
Sanchita Devi <thesanchitadevi@gmail.com>
2025-02-07 23:24:04 +0600

    Optimize Fonts, Improve SEO with Metadata, and Use Import Aliases

--- src/app/(withCommonLayout)/about/page.js
+++ src/app/(withCommonLayout)/about/page.js
@@ -1,5 +1,10 @@
 import styles from "./About.module.css";
 
+export const metadata = {
+  title: "About Us",
+  description: "This is the about page",
+};
+
 const AboutPage = () => {
   return <div className={styles.text_color}>This is About page</div>;
 };
--- src/app/(withCommonLayout)/gallery/page.js
+++ src/app/(withCommonLayout)/gallery/page.js
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import image from "../../assets/next.jpg";
+import image from "@/assets/next.jpg";
 
 const GalleryPage = () => {
   return (
