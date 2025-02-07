6605bb6dd9f256b205a4621200754de612e72982
Sanchita Devi <thesanchitadevi@gmail.com>
2025-02-08 03:00:30 +0600

    Advance Data Fetching Strategies with SSR

--- src/app/(withCommonLayout)/products/page.js
+++ src/app/(withCommonLayout)/products/page.js
@@ -3,20 +3,14 @@ import ProductCard from "@/components/Product/ProductCard";
 const ProductsPage = async () => {
   // Data fetching
   const res = await fetch("http://localhost:5000/products", {
-    // Data fetching with cache control
-    // cache: "force-cache", // this will fetch the data and store it in the cache as CDN (Content Delivery Network) cache for future use
-
-    // Data revalidation => after deploying the app, the data will be revalidated every 5 seconds means build/update the data manually. ISR (Incremental Static Regeneration) will be used. It only works in fetch function.
-    next: {
-      revalidate: 5,
-    },
+    cache: "no-store", // as per user req data will fetch dynamically without storing in cache
   });
   const products = await res.json();
 
   return (
     <div className="text-center">
       <h1 className="text-3xl font-bold py-5">Products </h1>
-      <div className="container mx-auto grid grid-cols-3 gap-4 ">
+      <div className="container mx-auto grid grid-cols-3 gap-4 my-5">
         {products.map((product) => (
           <ProductCard key={product.id} product={product} />
         ))}
